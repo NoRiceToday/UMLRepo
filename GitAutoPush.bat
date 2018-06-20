d44767bcc470ee885ce09be9f729f21e67058d49
Niklas Betten <niklasbetten98@gmail.com>
2018-06-20 15:37:26 +0200

    auto push

--- GitAutoPush.bat
+++ GitAutoPush.bat
@@ -2,7 +2,7 @@
 	
 	:: Navigate to the directory you wish to push to GitHub
 	::Change <path> as needed. Eg. D:\Dokumente\UMLRepo\UMLRepo
-	cd D:\UMLGit\UMLRepo\UMLRepo
+	cd C:\Users\Einstein\Documents\UMLRepo
 	
 	::Initialize GitHub
 	git init
